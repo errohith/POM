46c07733b2b23474ba8adacd9f989957d7fcb17e
Rohith <Rohith@host.docker.internal>
2019-12-15 16:32:44 +0530

    Final

--- src/main/java/runner/RunCuckes.java
+++ src/main/java/runner/RunCuckes.java
@@ -1,11 +1,10 @@
 package runner;
 
-import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
 
-@RunWith(Cucumber.class)
+
+
 @CucumberOptions(features = "./src/main/java/LearningCum", glue="Steps" , monochrome=false, tags = "@Smoke , @Sanity") //"@Sanity and @Smoke"
 
 public class RunCuckes {
--- src/main/java/stepsCommon/RunJunit.java
+++ src/main/java/stepsCommon/RunJunit.java
@@ -1,11 +1,10 @@
 package stepsCommon;
 
-import org.junit.runner.RunWith;
+
 
 import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
 
-@RunWith(Cucumber.class)
+
 @CucumberOptions(features = "./src/main/java/testCaseFeatures", 
 						glue = {"pagesPOM","stepsCommon"},
 						//tags= {"@smoke or @sanity"},
